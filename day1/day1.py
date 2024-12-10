fc6ae39e3c4d96872ae89f9120e5c046d29fc8a2
Ac5000 <15003210+Ac5000@users.noreply.github.com>
2024-12-09 21:32:05 -0500

    Finished part 2 and fixed incorrect constant formatting.

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,6 @@
 """Code for day 1."""
 
+# PART 1
 # Take input and get 2 lists, left and right. (Separator is spaces)
 left_list: list[int] = []
 right_list: list[int] = []
@@ -17,8 +18,21 @@ right_list.sort()
 # For each pair:
 #   Get absolute of subtraction.
 #   Add return to global.
-TOTAL_DISTANCE = 0
+total_distance = 0
 for l, r in zip(left_list, right_list):
-    TOTAL_DISTANCE = TOTAL_DISTANCE + abs(l - r)
+    total_distance = total_distance + abs(l - r)
 
-print(TOTAL_DISTANCE)
+print(total_distance)
+
+# PART 2
+# Didn't realize there were parts...
+
+# For each item in left list:
+#   Get count of item in right list.
+#   Multiply item by count.
+#   Add return to global.
+total_similarity = 0
+for i in left_list:
+    total_similarity = total_similarity + (i * right_list.count(i))
+
+print(total_similarity)
